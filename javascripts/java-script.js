4102f54b91a4d3d714a385c7861fa6af3f54dac3
nonitut <nona.blogg@gmail.com>
2022-01-13 14:57:50 +0300

    ih

---
+++ javascripts/java-script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() { //пишем прогрузку, чтоб точно все прогрузилось в оперативную память, поочередно читает, чтоб смог найти и прочитать
+console.log( 'готов!' );
+}); // Мы говорим: прогрузи документ с функцией, в консоль и отобрази готово
+$(document).ready(function() {
+  $("#demo").html("Hello, World!");
+});
+  // в документе html мы находим функцию, с айди #demo и передаем через html  слово Hello, World!, то есть текст написан в js
+  //сначала нашли айди которое заранее написали, потом функцию написали, html("Hello, World!") - можем написать переменную в  js
+  //внимательно следите за скобками
+
+    $(document).ready(function(){
+      $("div").click(function(){
+        $(this).addClass("black");
+      });
+    }); // при клике на див наша функция начинает работать - меняет цвет на черный
+    $(document).ready(function(){
+	     $(".blue").click(function(){
+		       $(this).css("background-color","orange");
+	});
+});
+// ".blue" - не забудьте точку поставить , указать что это класс
+$(document).ready(function(){
+   $(".mel").click(function(){
+       $(".ril").css("background-color","pink");
+});
+});
